4578fd2dcc077f3378ad76e6d46cdf6afb7c4e6b
torquecoder <torquecode@gmail.com>
2017-04-17 14:28:53 +0530

    API added

--- new_contest_api.py
+++ new_contest_api.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import urllib2
 import json
 import os
+from time import strptime,strftime,mktime,gmtime,localtime
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,7 +13,7 @@ api = Api(app)
 '''
 scraping codechef and creating a dictionary for json object
 '''
-
+'''
 url = 'https://www.codechef.com/contests'
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page,"html.parser")
@@ -40,6 +41,39 @@ for contest in upcoming_contests[1:]:
     end_time = event_details_row[3].string
     contest_url = "www.codechef.com/" + code
     resultSet["upcoming_contests"].append({"code":code,"platform":"codechef","name":name,"start_time":start_time,"end_time":end_time,"contest_url":contest_url})
+
+'''
+page = urlopen("http://www.codechef.com/contests")
+soup = BeautifulSoup(page, "html.parser")
+
+statusdiv = soup.findAll("table", attrs = {"class": "dataTable"})
+headings = soup.findAll("h3")
+contest_tables = {"Future Contests": [], "Present Contests": []}
+for i in range(len(headings)):
+    if headings[i].text != "Past Contests":
+        contest_tables[headings[i].text] = statusdiv[i].findAll("tr")[1:]
+
+for upcoming_contest in contest_tables["Future Contests"]:
+    details = upcoming_contest.findAll("td")
+    start_time = strptime(details[2].text, "%d %b %Y %H:%M:%S")
+    end_time = strptime(details[3].text, "%d %b %Y %H:%M:%S")
+    duration = get_duration(int((mktime(end_time) - mktime(start_time)) / 60))
+    posts["upcoming"].append({"Name":  details[1].text,
+                              "url": "http://www.codechef.com" + details[1].a["href"],
+                              "StartTime": strftime("%a, %d %b %Y %H:%M", start_time),
+                              "EndTime": strftime("%a, %d %b %Y %H:%M", end_time),
+                              "Duration": duration,
+                              "Platform": "CODECHEF"})
+
+for present_contest in contest_tables["Present Contests"]:
+    details = present_contest.findAll("td")
+    end_time = strptime(details[3].text, "%d %b %Y %H:%M:%S")
+    posts["ongoing"].append({"Name":  details[1].text,
+                             "url": "http://www.codechef.com" + details[1].a["href"],
+                             "EndTime": strftime("%a, %d %b %Y %H:%M", end_time),
+                             "Platform": "CODECHEF"})
+
+
 
 
 '''
